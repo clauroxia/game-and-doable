e5f64dfacd23a1a0bc60d8ec27c1aa3e58c47e8f
Claudia Berríos <cberriosledesma@gmail.com>
2024-06-09 10:22:57 -0500

    add logout service and logic to keep token and user in localStorage

--- src/app/doable/data-access/auth.service.ts
+++ src/app/doable/data-access/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { NEVER, Subject, catchError, switchMap, tap } from 'rxjs';
+import { NEVER, Observable, Subject, catchError, switchMap, tap } from 'rxjs';
 import { AuthState, Credentials, TokenResponse } from '../interfaces';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
@@ -10,10 +10,13 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 export class AuthService {
   private http = inject(HttpClient);
   private apiUrl = 'https://doable-api-production.up.railway.app';
+  private tokenKey = 'user-token';
+  private user = 'user';
 
   // sources
   signup$ = new Subject<Credentials>();
   login$ = new Subject<Credentials>();
+  logout$ = new Subject<void>();
 
   // state
   state = signal<AuthState>({
@@ -28,13 +31,24 @@ export class AuthService {
   error = computed(() => this.state().error);
 
   constructor() {
+    const token = localStorage.getItem(this.tokenKey);
+    const user = localStorage.getItem(this.user);
+    console.log(token, user);
+    if(token && user) {
+      this.setToken(token);
+      this.setUser(user);
+    }
+     
     // reducers
     this.signup$.pipe(
       takeUntilDestroyed(),
       tap(() => this.setError(null)),
       switchMap((credentials) =>
         this.http.post<TokenResponse>(this.apiUrl + '/signup', credentials).pipe(
-          tap(() => this.setUser(credentials)),
+          tap(() => {
+            this.setUser(credentials.email);
+            localStorage.setItem(this.user, credentials.email);
+          }),
           catchError((errorResponse) => {
             console.log(errorResponse);
             this.setError(errorResponse.error.errors.join());
@@ -46,6 +60,7 @@ export class AuthService {
     .subscribe((response) => {
       const { token } = response;
       this.setToken(token);
+      localStorage.setItem(this.tokenKey, token);
     })
 
     this.login$.pipe(
@@ -53,7 +68,10 @@ export class AuthService {
       tap(() => this.setError(null)),
       switchMap((credentials) =>
         this.http.post<TokenResponse>(this.apiUrl + '/login', credentials).pipe(
-          tap(() => this.setUser(credentials)),
+          tap(() => {
+            this.setUser(credentials.email);
+            localStorage.setItem(this.user, credentials.email);
+          }),
           catchError((errorResponse) => {
             console.log(errorResponse);
             this.setError(errorResponse.error.errors.join());
@@ -65,17 +83,36 @@ export class AuthService {
     .subscribe((response) => {
       const { token } = response;
       this.setToken(token);
+      localStorage.setItem(this.tokenKey, token);
     })
+
+    this.logout$
+      .pipe(
+        takeUntilDestroyed(),
+        tap(() => {
+          this.resetState();
+          localStorage.removeItem(this.tokenKey);
+          localStorage.removeItem(this.user);
+        }),
+        switchMap(() => 
+          this.http.delete(this.apiUrl + '/logout', {
+            headers: {
+              Authorization: `Token token=${this.token()}`
+            }
+          })
+        )
+      )
+      .subscribe(() => this.setToken(null))
   }
 
   private setToken(token: AuthState['token']) {
     this.state.update((prev) => ({...prev, token}));
   }
 
-  private setUser(credentials: Credentials) {
+  private setUser(user: string) {
     this.state.update((prev) => ({
       ...prev,
-      currentUser: {email: credentials.email}
+      currentUser: {email: user}
     }));
   }
 
@@ -84,5 +121,13 @@ export class AuthService {
       ...prev,
       error,
     }))
+  }
+
+  private resetState() {
+    this.state.update((prev) => ({ 
+      ...prev,
+      currentUser: null,
+      error: null
+    }));
   }
 }
