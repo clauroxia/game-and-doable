318bf452fc54ad1cd1447cc352d7dc2b0f393d6b
Claudia Berríos <cberriosledesma@gmail.com>
2024-06-08 20:47:01 -0500

    create signup component

---
+++ src/app/doable/signup/signup.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import { ButtonComponent } from '../../shared/components/ui/button/button.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../data-access/auth.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [ButtonComponent, ReactiveFormsModule],
+  template: `
+    <form [formGroup]="form" (ngSubmit)="onSubmit()" class="form">
+      <div class="group">
+        <label for="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="user@example.com"
+          formControlName="email"
+        />
+      </div>
+      <div class="group">
+        <label for="password">Password</label>
+        <input id="password" type="password" formControlName="password" />
+      </div>
+      <app-button textButton="Create" type="submit" />
+    </form>
+  `,
+  styleUrl: './signup.component.css',
+})
+export class SignupComponent {
+  fb = inject(FormBuilder);
+  authService = inject(AuthService);
+
+  form = this.fb.nonNullable.group({
+    email: [''],
+    password: [''],
+  });
+
+  onSubmit() {
+    console.log(this.form.getRawValue());
+    this.authService.signup$.next(this.form.getRawValue());
+  }
+}
