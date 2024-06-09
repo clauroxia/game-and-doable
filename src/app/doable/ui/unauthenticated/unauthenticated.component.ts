018325d447c1e88280545642ecc2457f80969cc7
Claudia Berríos <cberriosledesma@gmail.com>
2024-06-09 00:48:15 -0500

    create unauthenticated component

--- src/app/doable/ui/unauthenticated/unauthenticated.component.ts
+++ src/app/doable/ui/unauthenticated/unauthenticated.component.ts
@@ -1,16 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { AuthService } from '../../data-access/auth.service';
+import { ButtonComponent } from '../../../shared/components/ui/button/button.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-unauthenticated',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, CommonModule, RouterModule],
   template: `
-    <p>
-      unauthenticated works!
-    </p>
+    <div class="container-auth">
+      <div class="frame-button">
+        <app-button
+          textButton="Login"
+          [buttonDoable]="doable"
+          [isActive]="isActive"
+          [isDisabled]="isActive"
+          (clicked)="showForm()"
+          routerLink="login"
+        />
+        <app-button
+          textButton="Signup"
+          [buttonDoable]="doable"
+          [isActive]="!isActive"
+          [isDisabled]="!isActive"
+          (clicked)="showForm()"
+          routerLink="signup"
+        />
+      </div>
+      <router-outlet />
+    </div>
   `,
-  styleUrl: './unauthenticated.component.css'
+  styleUrl: './unauthenticated.component.css',
 })
 export class UnauthenticatedComponent {
+  doable = true;
+  isActive = true
+  authService = inject(AuthService);
 
+  constructor() {
+    effect(() => console.log(this.authService.state()));
+  }
+
+  showForm() {
+    this.isActive = !this.isActive;
+  }
 }
